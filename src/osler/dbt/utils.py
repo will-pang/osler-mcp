0b51d2ab0c00fdc124a14fdfad7d503e04e53793
will-astranahealth <william.pang@aya.yale.edu>
2025-12-01 17:47:00 -0800

    Minor additon

--- src/osler/dbt/utils.py
+++ src/osler/dbt/utils.py
@@ -37,7 +37,9 @@ def run_dbt_command(cmd: list[str], cwd: str, dataset_name: str) -> None:
             "--profile",
             dataset_name,
         ]
-        result = subprocess.run(add_profiles_specification_to_cmd, cwd=cwd, check=True, text=True)
+        result = subprocess.run(
+            add_profiles_specification_to_cmd, capture_output=True, cwd=cwd, check=True, text=True
+        )
         logger.info(f"✅ dbt {cmd[1:]} completed successfully")
         return result
     except subprocess.CalledProcessError as e:
@@ -48,3 +50,31 @@ def run_dbt_command(cmd: list[str], cwd: str, dataset_name: str) -> None:
     except FileNotFoundError:
         logger.info("❌ dbt command not found. Please ensure dbt is installed.")
         raise typer.Exit(1)
+
+
+def get_dbt_model_lineage(table_name, direction, depth):
+    dataset_name = "tuva-project-demo"
+    cwd = _DBT_PROJECT_ROOT / dataset_name
+
+    if direction == "parent":
+        lineage_arg = f"{depth}+{table_name}"
+    elif direction == "children":
+        lineage_arg = f"{table_name}+{depth}"
+
+    cmd = ["dbt", "ls", "-s", lineage_arg]
+
+    result = run_dbt_command(cmd, cwd, dataset_name)
+
+    lines = result.stdout.strip().split("\n")
+    models = [
+        line
+        for line in lines
+        if not line.startswith("0")
+        and "Running with dbt" not in line
+        and "target not specified" not in line
+        and "Registered adapter" not in line
+        and "Found" not in line
+    ]
+    models_lst = "\n".join(models)
+
+    return models_lst
