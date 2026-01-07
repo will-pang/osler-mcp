6a34eeb61aac5ebd8ae2a9ca96b25741f5218e62
will-astranahealth <william.pang@aya.yale.edu>
2026-01-06 21:19:03 -0800

    Update readmission_rate_by_ms_drg.sql

--- benchmarks/evals/tuva_project_demo/golden_query/readmission_rate_by_ms_drg.sql
+++ benchmarks/evals/tuva_project_demo/golden_query/readmission_rate_by_ms_drg.sql
@@ -3,25 +3,28 @@ Last Fetched: January 2, 2026
 Source: https://thetuvaproject.com/data-marts/readmissions
 */
 
-with readmit as 
-(
-select
-rs.ms_drg_code
-,drg.ms_drg_description
-, sum(case when index_admission_flag = 1 then 1 else 0 end) as index_admissions
-, sum(case when index_admission_flag = 1 and unplanned_readmit_30_flag = 1 then 1 else 0 end) as readmissions
-from readmissions.readmission_summary rs
-left join terminology.ms_drg drg on rs.ms_drg_code = drg.ms_drg_code
-group by 
-rs.ms_drg_code
-,drg.ms_drg_description
+with readmit as (
+  select
+    rs.drg_code as ms_drg_code,
+    drg.ms_drg_description,
+    sum(case when index_admission_flag = 1 then 1 else 0 end) as index_admissions,
+    sum(case when index_admission_flag = 1
+             and unplanned_readmit_30_flag = 1 then 1 else 0 end) as readmissions
+  from readmissions.readmission_summary rs
+  left join terminology.ms_drg drg
+    on rs.drg_code = drg.ms_drg_code
+  group by
+    rs.drg_code,
+    drg.ms_drg_description
 )
-
-select 
-ms_drg_code
-,ms_drg_description
-,index_admissions
-,readmissions
-,case when readmissions = 0 then 0 else readmissions / index_admissions end as readmission_rate
+select
+  ms_drg_code,
+  ms_drg_description,
+  index_admissions,
+  readmissions,
+  case
+    when index_admissions = 0 then 0
+    else readmissions / index_admissions
+  end as readmission_rate
 from readmit
 order by index_admissions desc
